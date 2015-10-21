44e44a8f0e0f57886e74210f5048f77cfeebcf18
Nikolas Skoufis <n.skoufis@gmail.com>
2015-10-21 22:58:16 +1100

    Fix linebreaks and listing options

--- endSemReport/calculation_pseudocode.py
+++ endSemReport/calculation_pseudocode.py
@@ -16,31 +16,25 @@ while not q.empty():
     player_one = Strategy(item.history1)
     player_two = Strategy(item.history2)
 
-    # Compute the moves that the strategies produce with the given
-    # histories, passing the opponent's history as well
+    # Compute the moves that the strategies produce with the given histories, passing the opponent's history as well
     move_one = player_one.next_move(player_two.history)
     move_two = player_two.next_move(player_one.history)
 
     # Compute the probability of no mistakes occurring
-    probability = item.probability * no_mistake_probability * \
-                  continuation_probability
+    probability = item.probability * no_mistake_probability * continuation_probability
 
     # If this maximum possible term size is larger than the threshold
     if probability * max_payoff > epsilon:
-        # Multiply this by the payoff from the outcome of
-        # a no-mistake round to find the term
+        # Multiply this by the payoff from the outcome of a no-mistake round to find the term
         term = probability * payoff(move_one, move_two)
         # Add the term to the expected payoff
         expected_payoff += term
-        # Add the probability along with the histories (including
-        # the new moves) back onto the queue
+        # Add the probability along with the histories (including the new moves) back onto the queue
         q.put(probability,
               item.history1 + move_one,
               item.history2 + move_two)
 
     else:
-        # The probability was too small, so don't add it back to
-        # the queue
+        # The probability was too small, so don't add it back to the queue
 
-    # Repeat this for each of the two one mistake cases and the
-    # two mistake case
+    # Repeat this for each of the two one mistake cases and the two mistake case
